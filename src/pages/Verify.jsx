cda1bb0cc7f198ef27914e29aac60e7e02c3dbb9
RakeshV0498 <rockr4674@gmail.com>
2024-06-18 10:57:25 +0530

    Updated verify component

--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import { FaCheckCircle } from "react-icons/fa";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 import { useNavigate, useParams } from "react-router-dom";
 import { userVerify } from "../apis/register";
 
@@ -9,17 +9,20 @@ const Verify = () => {
 
   const { verificationToken } = useParams();
   const [verificationMessage, setVerificationMessage] = useState("");
+  const [verificationSuccess, setVerificationSuccess] = useState(false);
 
   useEffect(() => {
-    console.log("Verification Token:", verificationToken); // Check if the token is extracted correctly
+    console.log("Verification Token:", verificationToken);
     const verifyEmail = async () => {
       try {
         const response = await userVerify(verificationToken);
-        console.log("Verification API Response:", response); // Check the response from the API
+        console.log("Verification API Response:", response);
         setVerificationMessage(response.msg);
+        setVerificationSuccess(true);
       } catch (error) {
-        console.error("Error verifying email:", error); // Log any errors that occur during the API call
+        console.error("Error verifying email:", error);
         setVerificationMessage("An error occurred while verifying your email");
+        setVerificationSuccess(false);
       }
     };
 
@@ -35,10 +38,19 @@ const Verify = () => {
     >
       <Row className="text-center">
         <Col>
-          <FaCheckCircle size={100} color="green" className="mb-4" />
-          <h2>User Verified</h2>
-          <p>Your account has been successfully verified.</p>
-          <p>{verificationMessage}</p>
+          {verificationSuccess ? (
+            <>
+              <FaCheckCircle size={100} color="green" className="mb-4" />
+              <h2>User Verified</h2>
+              <p>Your account has been successfully verified.</p>
+            </>
+          ) : (
+            <>
+              <FaTimesCircle size={100} color="red" className="mb-4" />
+              <h2>Verification Failed</h2>
+              <p>{verificationMessage}</p>
+            </>
+          )}
           <Button
             variant="primary"
             size="lg"
