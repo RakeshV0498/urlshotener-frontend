714f042cce9b48606e81773503dc9faa10430983
RakeshV0498 <rockr4674@gmail.com>
2024-06-19 07:32:32 +0530

    Updated Footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -5,46 +5,23 @@ const Footer = () => {
   return (
     <footer className="bg-light py-4 mt-auto">
       <Container>
-        <Row>
+        <Row className="justify-content-between">
           <Col md={4} className="text-center mb-3 mb-md-0">
             <h5>Follow Us</h5>
-            <a href="https://facebook.com" className="text-dark mx-2">
-              <FaFacebook size={24} />
-            </a>
-            <a href="https://twitter.com" className="text-dark mx-2">
-              <FaTwitter size={24} />
-            </a>
-            <a href="https://instagram.com" className="text-dark mx-2">
-              <FaInstagram size={24} />
-            </a>
-            <a href="https://linkedin.com" className="text-dark mx-2">
-              <FaLinkedin size={24} />
-            </a>
-          </Col>
-          <Col md={4} className="text-center mb-3 mb-md-0">
-            <h5>Quick Links</h5>
-            <ul className="list-unstyled">
-              <li>
-                <a href="#home" className="text-dark">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#products" className="text-dark">
-                  Products
-                </a>
-              </li>
-              <li>
-                <a href="#about" className="text-dark">
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a href="#contact" className="text-dark">
-                  Contact
-                </a>
-              </li>
-            </ul>
+            <div className="d-flex justify-content-center">
+              <a href="https://facebook.com" className="text-dark mx-2">
+                <FaFacebook size={24} />
+              </a>
+              <a href="https://twitter.com" className="text-dark mx-2">
+                <FaTwitter size={24} />
+              </a>
+              <a href="https://instagram.com" className="text-dark mx-2">
+                <FaInstagram size={24} />
+              </a>
+              <a href="https://linkedin.com" className="text-dark mx-2">
+                <FaLinkedin size={24} />
+              </a>
+            </div>
           </Col>
           <Col md={4} className="text-center">
             <h5>Contact Us</h5>
@@ -55,7 +32,9 @@ const Footer = () => {
         </Row>
         <Row className="mt-3">
           <Col className="text-center">
-            <p className="mb-0">&copy; 2024 My Website. All rights reserved.</p>
+            <p className="mb-0">
+              &copy; {new Date().getFullYear()} Miniurl. All rights reserved.
+            </p>
           </Col>
         </Row>
       </Container>
